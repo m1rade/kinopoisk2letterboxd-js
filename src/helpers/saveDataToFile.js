afa4cadac297cc137227fbebaa293390a758db57
m1rade <forx98@gmail.com>
2023-08-18 18:54:05 +0300

    save data to hard drive

--- src/helpers/saveDataToFile.js
+++ src/helpers/saveDataToFile.js
@@ -3,22 +3,27 @@ import { join } from "path";
 import { fileDirEnvVariables } from "../utils/fileDirEnvVariables.js";
 
 export async function saveDataToFile(data) {
+    const { __dirname } = fileDirEnvVariables(import.meta.url);
+    const dirPath = join(__dirname, "..", "..", "data");
+    const filePath = join(dirPath, "films.csv");
+
     return new Promise((resolve, rejected) => {
         if (!data.length) return rejected("Args haven't been provided");
 
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, err => {
+                if (err) return rejected(err);
+            });
 
-        const { __dirname } = fileDirEnvVariables(import.meta.url);
-        const dirPath = join(__dirname, "..", "..", "data");
-        const filePath = join(dirPath, "films.csv");
-
-        fs.mkdir(dirPath, (err) => {
-            if (err) throw err;
-
-            fs.writeFile(filePath, data, (err) => {
-                if (err) throw err;
-
-                return resolve();
-            })
-        });
+            fs.writeFileSync(filePath, data, err => {
+                if (err) return rejected(err);
+            });
+            return resolve(data);
+        } else {
+            fs.writeFileSync(filePath, data, err => {
+                if (err) return rejected(err);
+            });
+            return resolve(data);
+        }
     });
 }
