cc1ac208cd80608d4e01e682ab7153e08ec730cf
m1rade <forx98@gmail.com>
2023-08-19 19:28:32 +0300

    add description

--- src/utils/fileDirEnvVariables.js
+++ src/utils/fileDirEnvVariables.js
@@ -1,8 +1,15 @@
-import {fileURLToPath} from "url";
 import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-export function fileDirEnvVariables(url) {
-    const __filename = fileURLToPath(url);
+/**
+ * The function creates a custom __dirname and __filename variables
+ * that work just like the global variables that are defined in CommonJs but not in ES module
+ * @param meta module's metadata
+ * @returns Object with the absolute path of the current module file and it's directory
+ */
+
+export function fileDirEnvVariables(meta) {
+    const __filename = fileURLToPath(meta.url);
 
     const __dirname = dirname(__filename);
 
--- src/utils/timeout.js
+++ src/utils/timeout.js
@@ -1,3 +1,12 @@
+/**
+ * Using for puppeteerAPI as page.waitForTimeout(ms) method is now obsolete
+ * Causes your script to wait for the given number of milliseconds
+ * 
+ * https://pptr.dev/api/puppeteer.page.waitfortimeout
+ * 
+ * @param {number} ms to wait
+ * @returns Promise<void>
+ */
 export async function timeout(ms) {
     return new Promise(res => setTimeout(res, ms));
 }
